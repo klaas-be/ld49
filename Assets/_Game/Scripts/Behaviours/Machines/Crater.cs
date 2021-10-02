856396a74852e97eaaa85e83e3992423e2a34fdd
Tekk <mj@tekk.one>
2021-10-02 19:45:37 +0200

    Crater and Camera

---
+++ Assets/_Game/Scripts/Behaviours/Machines/Crater.cs
@@ -0,0 +1,39 @@
+using _Game.Scripts.Behaviours;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crater : Machine
+{
+    [SerializeField] Animator animator;
+    [SerializeField] Transform DropPoint;
+    [SerializeField] float throwForce;
+    [SerializeField] float throwDelay;
+
+    ElementComponent currentElement;
+
+    public override void Use(ElementComponent elementComponent)
+    {
+        canBeUsed = false;
+        currentElement = elementComponent;
+        currentElement.GetComponent<Collider>().enabled = false;
+
+        //Throw in Crater
+        Rigidbody rigidbody = currentElement.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        currentElement.transform.SetParent(null);
+        rigidbody.AddForce((DropPoint.position - rigidbody.transform.position) * throwForce, ForceMode.Impulse);
+
+        Destroy(currentElement.gameObject, 2f);
+        currentElement = null;
+
+        //animator.SetTrigger("CraterTrigger");
+        StartCoroutine(ThrowProcess());
+    }
+
+    private IEnumerator ThrowProcess()
+    {
+        yield return new WaitForSeconds(throwDelay);
+        canBeUsed = true;
+    }
+}
