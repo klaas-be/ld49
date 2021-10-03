290bf8c0c8791572c6da7e41c800d2c23580de33
Tekk <mj@tekk.one>
2021-10-03 22:52:27 +0200

    Level01

--- Assets/_Game/Scripts/Classes/CombineRecipes.cs
+++ Assets/_Game/Scripts/Classes/CombineRecipes.cs
@@ -21,28 +21,28 @@ namespace _Game.Scripts.Classes
             }
 
             //elec steel + helium = lightsteel
-            if (A.elementType == ElementType.Steel && A.elementStatus == ElementStatus.Shocked &&
-                B.elementType == ElementType.Helium && B.elementStatus == ElementStatus.Default)
+            if (A.elementType == ElementType.Steel      && A.elementStatus == ElementStatus.Shocked &&
+                B.elementType == ElementType.Helium     && B.elementStatus == ElementStatus.Default)
             {
                 return new Element(ElementType.Lightsteel);
             }
 
             //star comp +star comp = whitestar
-            if (A.elementType == ElementType.Star && A.elementStatus == ElementStatus.Compressed &&
-                B.elementType == ElementType.Star && B.elementStatus == ElementStatus.Compressed)
+            if (A.elementType == ElementType.Star       && A.elementStatus == ElementStatus.Compressed &&
+                B.elementType == ElementType.Star       && B.elementStatus == ElementStatus.Compressed)
             {
                 return new Element(ElementType.Whitestar);
             }
             //star burn +star elec = whitestar
-            if (A.elementType == ElementType.Star && A.elementStatus == ElementStatus.Burned &&
-                B.elementType == ElementType.Star && B.elementStatus == ElementStatus.Shocked)
+            if (A.elementType == ElementType.Star       && A.elementStatus == ElementStatus.Burned &&
+                B.elementType == ElementType.Star       && B.elementStatus == ElementStatus.Shocked)
             {
                 return new Element(ElementType.Shootingstar);
             }
 
             //TEST STEEL STEEL 
-            if (A.elementType == ElementType.Steel && A.elementStatus == ElementStatus.Default &&
-                B.elementType == ElementType.Steel && B.elementStatus == ElementStatus.Default)
+            if (A.elementType == ElementType.Steel      && A.elementStatus == ElementStatus.Default &&
+                B.elementType == ElementType.Steel      && B.elementStatus == ElementStatus.Default)
             {
                 return new Element(ElementType.Steel);
             }
