ee9262bcd9ac404b5957d34d22cffb9d9ef2a024
Klaas <klaas@tinyroar.de>
2021-10-03 22:43:25 +0200

    UPDATE added display for requested item, spawn point for queue

--- Assets/_Game/Scripts/Behaviours/RecipeQueue.cs
+++ Assets/_Game/Scripts/Behaviours/RecipeQueue.cs
@@ -3,8 +3,8 @@ using System.Linq;
 using _Game.Scripts.Classes;
 using _Game.Scripts.ScriptableObjects;
 using NaughtyAttributes;
-using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _Game.Scripts.Behaviours
 {
@@ -12,6 +12,13 @@ namespace _Game.Scripts.Behaviours
     {
         public List<Recipe> recipePool;
         public List<Recipe> queue;
+
+        public Vector3 spawnPoint;
+        public float throwForce; 
+        public Vector3 forceVector = new Vector3(0,1,-1);
+
+        
+        public ElementDisplay requestElementDisplay; 
 
         [Button]
         public void PickRecipeToQueue()
@@ -21,25 +28,45 @@ namespace _Game.Scripts.Behaviours
             queue.Add(picked);
             foreach (var e in picked.toSpawn)
             {
-                var element = ElementSpawner.Instance.Spawn(e.elementType, transform.position); 
-                GetComponent<Crater>().ThrowNewElement(element);
+                var element = ElementSpawner.Instance.Spawn(e.elementType, transform.position);
+                element.transform.position = spawnPoint; 
+                Rigidbody rigidbody = element.GetComponent<Rigidbody>();
+                rigidbody.isKinematic = false;
+                rigidbody.AddForce(forceVector * throwForce, ForceMode.Impulse);
             }
+            DisplayRequested();
         }
+        
 
         public void CheckIfRequested(Element element)
         {
             var completed = queue.Find(recipe => recipe.requested.elementType == element.elementType);
             if(completed != null)
                 queue.Remove(completed); 
+            DisplayRequested();
         }
 
         [Button()]
         public void DisplayRequested()
         {
+            if (queue.Count == 0)
+            {
+                requestElementDisplay.gameObject.SetActive(false);
+                return;
+            }
            var requestedElement =  queue.First().requested;
            var iconToDisplay =
                ElementSpawner.Instance.ElementSettings.Find(settings => settings.Type == requestedElement.elementType).icon; 
            Debug.Log(requestedElement);
+           requestElementDisplay.element = requestedElement;
+           requestElementDisplay.Icon.sprite = iconToDisplay;
+           requestElementDisplay.gameObject.SetActive(true);
+
+        }
+        
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(spawnPoint, 0.2f);
         }
     }
 }
---
+++ Assets/_Game/Scripts/UI/TimeEventController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Game.Scripts.UI
+{
+    public class TimeEventController : MonoBehaviour
+    {
+        public float currentTime;
+
+        public List<TimeEvent> TimeEvents = new List<TimeEvent>();
+
+        private void Awake()
+        {
+            List<Time>
+        }
+
+        private void Update()
+        {
+            currentTime += Time.deltaTime; 
+            
+            
+        }
+    }
+
+    public struct TimeEvent
+    {
+        public float time; 
+        UnityEvent e; 
+    }
+}
