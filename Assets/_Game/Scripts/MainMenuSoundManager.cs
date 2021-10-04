97b3f6e29ac8aa5adcc70eacd28674de9c597a8e
Alex <alexander.kring@web.de>
2021-10-04 17:32:19 +0200

    mainmenusoundsmaybe

---
+++ Assets/_Game/Scripts/MainMenuSoundManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSoundManager : MonoBehaviour
+{
+    public static MainMenuSoundManager instance;
+
+    [SerializeField]
+    List<AudioSource> audioSources;
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    public void SetSound(bool state) {
+        for (int i = 0; i < audioSources.Count; i++) {
+            audioSources[i].mute = state;
+        }
+    }
+}
--- Assets/_Game/Scripts/StateHolder.cs
+++ Assets/_Game/Scripts/StateHolder.cs
@@ -6,10 +6,19 @@ using Assets.TekkTech.Scripts.Language;
 public class StateHolder : MonoBehaviour
 {
 
+    public static StateHolder instance;
+
     public bool playSoundSetting = true;
 
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
     public void ToggleSound() {
         playSoundSetting = !playSoundSetting;
+        MainMenuSoundManager.instance.SetSound(playSoundSetting);
     }
 
 
